7c424e255e8eceb27ae56f1d4085e7140f765f61
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-03-01 00:47:49 +0800

    v1.0

---
+++ addarray.php
@@ -0,0 +1,7 @@
+<?php
+$odd_numbers = [1,3,5,7,9]; // Explnation: This is an array of odd numbers
+$odd_numbers[5] = 11; // Explnation: This adds 11 to the end of the array
+print_r($odd_numbers); 
+?>
+
+//Output: Array ( [0] => 1 [1] => 3 [2] => 5 [3] => 7 [4] => 9 [5] => 11 )
